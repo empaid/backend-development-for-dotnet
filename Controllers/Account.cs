8469a72876ca70101af53e6f15c625e63e909787
empaid <empaid@empaids-MBP.modem>
2023-11-20 15:59:33 +1100

    Strongly Typed Form

--- Controllers/Account.cs
+++ Controllers/Account.cs
@@ -1,3 +1,4 @@
+using backend_development_for_dotnet.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace backend_development_for_dotnet.Controllers
@@ -13,6 +14,17 @@ namespace backend_development_for_dotnet.Controllers
         public IActionResult LoginPost(string username, string password){
             ViewBag.Username = username;
             ViewBag.Password = password;
+            return View();
+        }
+        public IActionResult StronglyTypedLogin()
+        {
+            return View();
+        }
+        public IActionResult LoginStrongPost(LoginViewModel login)
+        {
+            ViewBag.Username = login.Username;
+            ViewBag.Password = login.Password;
+            ViewBag.Message = "Strongly Typed Form";
             return View();
         }
     }
---
+++ Models/LoginViewModel.cs
@@ -0,0 +1,8 @@
+namespace backend_development_for_dotnet.Models
+{
+    public class LoginViewModel
+    {
+        public string? Username {get; set;}
+        public string? Password {get; set;}
+    }
+}
