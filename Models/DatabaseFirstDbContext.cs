4bef04630bbb16d5345ee95b659b253c8a101aa0
empaid <empaid@empaids-MBP.modem>
2023-12-06 20:16:55 +1100

    DatabaseFirstApproach

---
+++ Models/DatabaseFirstDbContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend_development_for_dotnet.Models;
+
+public partial class DatabaseFirstDbContext : DbContext
+{
+    public DatabaseFirstDbContext()
+    {
+    }
+
+    public DatabaseFirstDbContext(DbContextOptions<DatabaseFirstDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseMySQL("server=localhost;database=DatabaseFirstDB;user=root;password=password");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("Product", "DatabaseFirstDB");
+
+            entity.Property(e => e.Name).HasMaxLength(10);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Models/Product.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace backend_development_for_dotnet.Models;
+
+public partial class Product
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+}
